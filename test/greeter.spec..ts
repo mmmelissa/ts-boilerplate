9e394715c195f1ec9beffb5f0d8bdd6bf46068c5
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-07-11 14:11:01 -0700

    Using npm scripts and webpack instead

--- test/greeter.spec..ts
+++ test/greeter.spec..ts
@@ -1,5 +1,6 @@
-describe('greeter', function(){
-    it('returns the right greeting', function() {
+import {Greeter} from '../src/greeter';
+describe('importing TS modules', () => {
+    it('can use imported code', () => {
         const greeter = new Greeter('folks');
         const message = greeter.greet();
         expect(message).toEqual('Howdy, folks');
